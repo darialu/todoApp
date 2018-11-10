1e8f1dcfef204551c8caa9d54fb4547e16faf1eb
Daria <dashika992@gmail.com>
2018-11-10 15:32:55 +0200

    Update base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://nodejs-app.cloudapp.net:8000'  
+  // baseUrl: 'http://nodejs-app.cloudapp.net:8000'  
+  baseUrl: 'http://darialu.info:8000'  
+
 };
